fe9fa36920c6b0b29ec945f8dd45624f6a7c4744
Tom Wallis <tom@tom-wallis.com>
2015-02-10 11:53:15 +0000

    Moved back to sqlite3. Whoops.

--- openmeditations/settings.py
+++ openmeditations/settings.py
@@ -58,7 +58,7 @@ WSGI_APPLICATION = 'openmeditations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'openmed.sqlite3'),
     }
 }
